79cfb0e1551fd07871fb0d7e98b079d565d64b3c
Буй Дык Туан <62297741+TuanBuiDucVolsu@users.noreply.github.com>
2020-12-10 14:56:24 +0400

    Update tstate.h

--- tstate.h
+++ tstate.h
@@ -7,21 +7,21 @@
 class tstate
 {
 private:
-	int max_num_state;
-	int num_state;
-	int max_num_pstates;
-	int num_pstate;
-	int max_num_tstates;
-	int num_tstate;
-	state* states;
-	state_status* tstates;
-	president_state* pstates;
+	int max_num_state;         //максимальное количество state
+	int num_state;             //  фактический номер state 
+	int max_num_pstates;       // максимальное количество president state
+	int num_pstate;            // фактический номер president state
+	int max_num_tstates;       // максимальное количество обновлен state
+	int num_tstate;            //фактический номер обновлен state
+	state* states;             //массив оригинальных state
+	state_status* tstates;     // массив обновлен
+	president_state* pstates;  // массив president state
 public:
-	tstate(int max_nst,int max_pst,int max_stt);
+	tstate(int max_nst,int max_pst,int max_stt);         ////конструкторы и деструкторы
 	~tstate();
 
-	void operator += (state& astate);
-	void operator +=(const state_status& ststate);
+	void operator += (state& astate);                       // перегрузка оператора += для добавления нового
+	void operator +=(const state_status& ststate); 
 	void operator +=(const president_state& ptstate);
 	void read_from_file(string filename);
 	void write_to_file(string filename);
